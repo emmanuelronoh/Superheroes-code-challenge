de1fd289f7fe5f2d36c30b86b7285ce162358490
emmanuel ronoh <eronoh036@gmail.com>
2024-10-10 21:09:46 +0300

    first commit

--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://super:12345@localhost:5432/superhero'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://super:12345@localhost:5432/superhero')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
